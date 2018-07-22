0ebd5e750459fc3346bdf8c451309913e380bd07
Ratko Trajkovski <ratko@interapptivelabs.com>
2018-07-23 00:47:50 +0200

    initial code push

---
+++ assets/js/app.js
@@ -0,0 +1,24 @@
+﻿
+
+$(document).ready(function () {
+
+    initTabs();
+})
+
+function initTabs() {
+    $(".tabs").each(function () {
+        var that = $(this);
+
+        var header = that.find(".tabs-header ul li a");
+        var content = that.find(".tabs-content ul li");
+        header.click(function (ev) {
+            ev.preventDefault();
+            $(this).closest("ul").find(".active").removeClass("active");
+            $(this).addClass("active");
+
+            content.hide();
+            content.eq(parseInt($(this).data("content"))).show();
+        })
+
+    })
+}
